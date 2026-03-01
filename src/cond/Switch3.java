63716c47bc000d859faa0e42b0b543bb6754540f
nz663633 <hyunjisim3@gmail.com>
2026-03-01 20:43:48 +0900

    docs: study switch statements and related examples

---
+++ src/cond/Switch3.java
@@ -0,0 +1,22 @@
+package cond;
+
+public class Switch3 {
+
+    public static void main(String[] args) {
+        int grade = 2;
+
+        int coupon;
+        switch (grade) {
+            case 1:
+                coupon = 1000;
+                break;
+            case 2: // break가 없으면 중단하지 않고 다음 case3 코드 실행
+            case 3:
+                coupon = 3000;
+                break;
+            default:
+                coupon = 500;
+        }
+        System.out.println("발급받은 쿠폰 " + coupon);
+    }
+}
